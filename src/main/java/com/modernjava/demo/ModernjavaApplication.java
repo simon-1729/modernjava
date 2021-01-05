55d7d6573ea38d8a39707243082edb94e951e475
Simon <dynamowebdesign@gmail.com>
2021-01-05 15:42:08 +0000

    Added project directories src etc

---
+++ src/main/java/com/modernjava/demo/ModernjavaApplication.java
@@ -0,0 +1,13 @@
+package com.modernjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernjavaApplication.class, args);
+	}
+
+}
