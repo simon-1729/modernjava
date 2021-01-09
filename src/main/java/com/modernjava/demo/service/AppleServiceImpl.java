84b53e89fb84ad7da27e2cc78e8f97164930718d
Simon <dynamowebdesign@gmail.com>
2021-01-09 14:49:22 +0000

    Removed unused import

--- src/main/java/com/modernjava/demo/service/AppleServiceImpl.java
+++ src/main/java/com/modernjava/demo/service/AppleServiceImpl.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.modernjava.demo.controller.ModernJavaController;
 import com.modernjava.demo.model.Apple;
 import com.modernjava.demo.model.AppleColor;
 
